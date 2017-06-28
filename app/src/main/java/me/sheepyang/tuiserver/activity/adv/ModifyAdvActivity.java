179113f1a782893cf1418ab43c61d085e9dfd9f6
SheepYang1993 <332594623@qq.com>
2017-06-28 18:30:37 +0800

    ~

--- app/src/main/java/me/sheepyang/tuiserver/activity/adv/ModifyAdvActivity.java
+++ app/src/main/java/me/sheepyang/tuiserver/activity/adv/ModifyAdvActivity.java
@@ -277,50 +277,55 @@ public class ModifyAdvActivity extends BaseActivity implements View.OnClickListe
                 showMessage(getString(R.string.not_open));
                 break;
             case R.id.btn_select_adv_image:
-                // 进入相册 以下是例子：用不到的api可以不写
-                PictureSelector.create(mActivity)
-                        .openGallery(PictureMimeType.ofImage())//全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()
-                        .theme(R.style.mypicture_default_style)//主题样式(不设置为默认样式) 也可参考demo values/styles下 例如：R.style.picture.white.style
-                        .maxSelectNum(1)// 最大图片选择数量 int
-                        .minSelectNum(1)// 最小选择数量 int
-                        .imageSpanCount(4)// 每行显示个数 int
-                        .selectionMode(PictureConfig.SINGLE)// 多选 or 单选 PictureConfig.MULTIPLE or PictureConfig.SINGLE
-                        .previewImage(true)// 是否可预览图片 true or false
-                        .previewVideo(false)// 是否可预览视频 true or false
-                        .enablePreviewAudio(false) // 是否可播放音频 true or false
-                        .compressGrade(Luban.THIRD_GEAR)// luban压缩档次，默认3档 Luban.THIRD_GEAR、Luban.FIRST_GEAR、Luban.CUSTOM_GEAR
-                        .isCamera(true)// 是否显示拍照按钮 true or false
-                        .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
-//                        .sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
-                        .setOutputCameraPath(Constants.IMAGE_FILE_PATH)// 自定义拍照保存路径,可不填
-                        .enableCrop(true)// 是否裁剪 true or false
-                        .compress(true)// 是否压缩 true or false
-                        .compressMode(LUBAN_COMPRESS_MODE)//系统自带 or 鲁班压缩 PictureConfig.SYSTEM_COMPRESS_MODE or LUBAN_COMPRESS_MODE
-//                        .glideOverride()// int glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
-                        .withAspectRatio(2, 1)// int 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
-                        .hideBottomControls(true)// 是否显示uCrop工具栏，默认不显示 true or false
-                        .isGif(false)// 是否显示gif图片 true or false
-                        .freeStyleCropEnabled(false)// 裁剪框是否可拖拽 true or false
-                        .circleDimmedLayer(false)// 是否圆形裁剪 true or false
-                        .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false   true or false
-                        .showCropGrid(true)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false    true or false
-                        .openClickSound(false)// 是否开启点击声音 true or false
-                        .selectionMedia(mImageSelectList)// 是否传入已选图片 List<LocalMedia> list
-                        .previewEggs(true)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中) true or false
-//                        .cropCompressQuality()// 裁剪压缩质量 默认90 int
-                        .compressMaxKB(300)//压缩最大值kb compressGrade()为Luban.CUSTOM_GEAR有效 int
-                        .compressWH(1000, 500) // 压缩宽高比 compressGrade()为Luban.CUSTOM_GEAR有效  int
-                        .cropWH(1000, 500)// 裁剪宽高比，设置如果大于图片本身宽高则无效 int
-                        .rotateEnabled(true) // 裁剪是否可旋转图片 true or false
-                        .scaleEnabled(true)// 裁剪是否可放大缩小图片 true or false
-//                        .videoQuality()// 视频录制质量 0 or 1 int
-//                        .videoSecond()// 显示多少秒以内的视频or音频也可适用 int
-//                        .recordVideoSecond()//视频秒数录制 默认60s int
-                        .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+            case R.id.iv_adv:
+                selectPhoto();
                 break;
             default:
                 break;
         }
+    }
+
+    private void selectPhoto() {
+        // 进入相册 以下是例子：用不到的api可以不写
+        PictureSelector.create(mActivity)
+                .openGallery(PictureMimeType.ofImage())//全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()
+                .theme(R.style.mypicture_default_style)//主题样式(不设置为默认样式) 也可参考demo values/styles下 例如：R.style.picture.white.style
+                .maxSelectNum(1)// 最大图片选择数量 int
+                .minSelectNum(1)// 最小选择数量 int
+                .imageSpanCount(4)// 每行显示个数 int
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选 PictureConfig.MULTIPLE or PictureConfig.SINGLE
+                .previewImage(true)// 是否可预览图片 true or false
+                .previewVideo(false)// 是否可预览视频 true or false
+                .enablePreviewAudio(false) // 是否可播放音频 true or false
+                .compressGrade(Luban.THIRD_GEAR)// luban压缩档次，默认3档 Luban.THIRD_GEAR、Luban.FIRST_GEAR、Luban.CUSTOM_GEAR
+                .isCamera(true)// 是否显示拍照按钮 true or false
+                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+//                        .sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+                .setOutputCameraPath(Constants.IMAGE_FILE_PATH)// 自定义拍照保存路径,可不填
+                .enableCrop(true)// 是否裁剪 true or false
+                .compress(true)// 是否压缩 true or false
+                .compressMode(LUBAN_COMPRESS_MODE)//系统自带 or 鲁班压缩 PictureConfig.SYSTEM_COMPRESS_MODE or LUBAN_COMPRESS_MODE
+//                        .glideOverride()// int glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+                .withAspectRatio(2, 1)// int 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+                .hideBottomControls(true)// 是否显示uCrop工具栏，默认不显示 true or false
+                .isGif(false)// 是否显示gif图片 true or false
+                .freeStyleCropEnabled(false)// 裁剪框是否可拖拽 true or false
+                .circleDimmedLayer(false)// 是否圆形裁剪 true or false
+                .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false   true or false
+                .showCropGrid(true)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false    true or false
+                .openClickSound(false)// 是否开启点击声音 true or false
+                .selectionMedia(mImageSelectList)// 是否传入已选图片 List<LocalMedia> list
+                .previewEggs(true)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中) true or false
+//                        .cropCompressQuality()// 裁剪压缩质量 默认90 int
+                .compressMaxKB(300)//压缩最大值kb compressGrade()为Luban.CUSTOM_GEAR有效 int
+                .compressWH(1000, 500) // 压缩宽高比 compressGrade()为Luban.CUSTOM_GEAR有效  int
+                .cropWH(1000, 500)// 裁剪宽高比，设置如果大于图片本身宽高则无效 int
+                .rotateEnabled(true) // 裁剪是否可旋转图片 true or false
+                .scaleEnabled(true)// 裁剪是否可放大缩小图片 true or false
+//                        .videoQuality()// 视频录制质量 0 or 1 int
+//                        .videoSecond()// 显示多少秒以内的视频or音频也可适用 int
+//                        .recordVideoSecond()//视频秒数录制 默认60s int
+                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
     }
 
     @Override
